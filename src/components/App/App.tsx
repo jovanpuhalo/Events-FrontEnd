3509d535e96a863572286ae78cff44cccb16a905
jovanpuhalo <jovan.puhalo86@gmail.com>
2021-02-09 16:12:15 +0100

    Instalacija potrebnih biblioteka: bootstrap, jQuery, popper.js, react-bootstrap, fontawesome

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
     <div className="App">
+      <FontAwesomeIcon icon={faHome} />
       React komponenta
     </div>
   );
